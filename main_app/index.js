6a4dfd02e5bac8d0b1682cd5bfc17ed9d040f641
Toni Pikkarainen <tonipikkarainen@Toni-MBP.home>
2021-01-16 11:52:46 +0200

    Access with ingress <-- ex 1.07

--- main_app/index.js
+++ main_app/index.js
@@ -1,10 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3001;
 const uuid = require("uuid");
 
 const randomString = uuid.v4();
+var status = "";
 
 const printRandomString = () => {
-  console.log(new Date(Date.now()).toLocaleString() + ": " + randomString);
+  status = Date(Date.now()).toLocaleString() + ": " + randomString;
+  console.log(status);
   setTimeout(printRandomString, 5000);
 };
 
 printRandomString();
+
+app.get("/", (req, res) => {
+  res.send(status);
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
